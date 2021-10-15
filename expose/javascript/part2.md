ea9dc3df14fffb9a947e4de7ccd61f41c42f932e
Jianghualu <jil184@ucsd.edu>
2021-10-15 12:04:08 -0700

    updated part 2md unfinish 2 and 3

---
+++ expose/javascript/part2.md
@@ -0,0 +1,11 @@
+1. line 12 print the value of i which is 3 because the length of price is 3, so the loop will run 3 times, and i increased from 0 to 3. Also var variable is a function scope, so it can be use anywhere and reached to line 12, therefore no error for line 12.
+   
+2. line 13 print the value of discountedPrice which is 150 because the variable is 
+3. 
+4. 
+5. 
+6. 
+7. . Also var variable is a function scope, so it can be use anywhere and reached to line 13, therefore no error for line 13.
+
+
+8. line 14 print the value of finalPrice which is 150 because the length of price is 3, so the loop will run 3 times, and i increased from 0 to 3. Also var variable is a function scope, so it can be use anywhere and reached to line 14, therefore no error for line 14.
--- expose/testing.js
+++ expose/testing.js
@@ -1,9 +1,23 @@
-function sumValues(num1, num2, add){
-    if(add) {
-        const result = 0; 
-        result = num1 + num2;
-        console.log('values added: ', result);
-    }else return;
-    console.log('final result: ', result);
+// function sumValues(num1, num2, add){
+//     if(add) {
+//         const result = 0; 
+//         result = num1 + num2;
+//         console.log('values added: ', result);
+//     }else return;
+//     console.log('final result: ', result);
+// }
+// sumValues(10, 10, true);
+
+function discountPrices(prices,discount) {
+    var discounted = [];
+    var finalPrice = 0;
+    for(var i = 0; i < prices.length; i++) {
+        var discountedPrice = prices[i] * (1 - discount);
+        finalPrice = Math.round(discountedPrice * 100) / 100;
+        discounted.push(finalPrice);
+    }
+    //console.log(i);
+    console.log(discountPrices);
+    return discounted;
 }
-sumValues(10, 10, true);
+discountPrices([100,200,300], 0.5);
